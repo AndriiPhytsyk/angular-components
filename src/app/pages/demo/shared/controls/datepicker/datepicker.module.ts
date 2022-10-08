74264c8ee1ddc76bc53e49a81d96aa79a3ee53cf
Andrii <phytsyk@gmail.com>
2022-10-08 20:27:38 +0300

    fix styles

--- src/app/pages/demo/shared/controls/datepicker/datepicker.module.ts
+++ src/app/pages/demo/shared/controls/datepicker/datepicker.module.ts
@@ -3,10 +3,11 @@ import { CommonModule } from '@angular/common';
 import { DatepickerComponent } from './datepicker.component';
 import { MatDatepickerModule } from '@angular/material/datepicker';
 import { FormsModule } from '@angular/forms';
+import { MatIconModule } from '@angular/material/icon';
 
 @NgModule({
   declarations: [DatepickerComponent],
-  imports: [CommonModule, FormsModule, MatDatepickerModule],
+  imports: [CommonModule, FormsModule, MatDatepickerModule, MatIconModule],
   exports: [DatepickerComponent]
 })
 export class DatepickerModule {}
